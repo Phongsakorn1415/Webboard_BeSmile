f281573d4167790a3d01130d7d6db867b789110b
Phongsakorn1415 <s6603052411032@email.kmutnb.ac.th>
2024-02-07 16:51:34 +0700

    Update index.php
    Update index.php :
    add navbar

--- index.php
+++ index.php
@@ -9,11 +9,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BeSmile Webboard</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
 <body>
-    <h1 style="text-align: center;">BeSmile WebBoard</h1>
-    <hr>
+    <div class="container-lg">
+    <h1 class="mt-3 mb-3" style="text-align: center;">BeSmile WebBoard</h1>
+    
+    <nav class="navbar navbar-expand-lg mb-3" style="background-color: #d3d3d3;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> Home</a>
+            <ul class="navbar-nav">
+                <?php
+                    if(!isset($_SESSION["id"])) {
+                ?>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
+                        </li>
+                <?php
+                    }else{
+                ?>
+                        <li class="nav-item dropdown">
+                            <a class="btn btn-outline-secondary btn-sm dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-person-lines-fill"></i> <?php echo $_SESSION["username"] ?>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+                            </ul>
+                        </li>
+                <?php
+                    }
+                ?>
+            </ul>
+        </div>
+    </nav>
+
     <form action="#">
         หมวดหมู่ : 
         <select>
@@ -51,6 +83,7 @@
             }
         ?>
     </ul>
+    </div>
 </body>
 
 </html>
